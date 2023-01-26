fa238fdd8a1c7efed3d51ed9669c7d87be69500f
mscottoc <michaeloconnor20@outlook.com>
2023-01-26 14:27:50 -0700

    Create hotel.py

---
+++ hotel.py
@@ -0,0 +1,14 @@
+# 1. Name:
+#      -your name-
+# 2. Assignment Name:
+#      Lab 04: Monopoly
+# 3. Assignment Description:
+#      -describe what this program is meant to do-
+# 4. What was the hardest part? Be as specific as possible.
+#      Was it the syntax of Python?
+#      Was it an aspect of the problem you are to solve?
+#      Was it the instructions or any part of the problem definition?
+#      Was it the submission process?
+# 5. How long did it take for you to complete the assignment?
+#      -total time in hours including reading the assignment and submitting the program-
+
